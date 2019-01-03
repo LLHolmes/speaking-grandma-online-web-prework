ab48e4e520839b2debafa5d0b8c1359d22ec1be2
Lana Holmes <lana.lee.holmes@gmail.com>
2019-01-03 18:18:38 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,12 @@
 def speak_to_grandma(string)
-  if string == 
+  if string == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  end
+  elsif string == string.upcase
+    return "NO, NOT SINCE 1938!"
+  end
+  else
+    return "HUH?! SPEAK UP, SONNY!"
 end
 
 # Whatever you say to grandma, she should respond with
